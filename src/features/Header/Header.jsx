0ff13a26cf8e49cdae18f3731c631826f45840b1
Sidney Russell <sidneyrussell58@gmail.com>
2025-08-14 18:05:15 -0700

    Set-up header.jsx

---
+++ src/features/Header/Header.jsx
@@ -0,0 +1,77 @@
+//Site header with logo, login, and searchbar
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import CreatePost from '../../features/posts/CreatePost';
+import './Header.css';
+
+const Header = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+
+    const [showCreatePost, setShowCreatePost] = useState(false);
+    const toggleCreatePost = () => {
+        setShowCreatePost(!showCreatePost);
+    };
+
+    const handleInputChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+        if (searchTerm.trim()) {
+            navigate(`/search?q=${encodeURIComponent(searchTerm)}`);
+            setSearchTerm('');
+        }
+    };
+
+    /*const clientId = 'EJ0qQsv53I1NBynzswhTAw';
+    const redirectUri = 'http://localhost:3000/authorize_callback';
+    const scope = 'read identity';*/
+
+    /*const loginWithReddit = () => {
+        const url = `https://www.reddit.com/api/v1/authorize?client_id=${clientId}&response_type=code&state=random_string&redirect_uri=${redirectUri}&duration=temporary&scope=${scope}`;
+        window.location.href = url;
+    };*/
+
+    const loginWithReddit = () => {
+        window.location.href = 'http://localhost:5000/auth/reddit';
+    };
+
+    return (
+        <div className='header'>
+            <button className='login' onClick={loginWithReddit}>
+                <span className='btn-text'>Login</span>
+            </button>
+
+            <button className='create-post-btn' onClick={toggleCreatePost}>
+                ➕ <span className='btn-text'>Create</span>
+            </button>
+            {showCreatePost && (
+                <div className='modal-backdrop' onClick={toggleCreatePost}>
+                    <div className='modal-content' onClick={e => e.stopPropagation()}>
+                        <CreatePost onClose={toggleCreatePost} />
+                    </div>
+                </div>
+            )}
+
+            <div className='logo-container'>
+                <img src='/images/logo.jpg' alt='Meddit Logo Full' className='logo' />
+                <img src='/images/logo-small.png' alt='Meddit Logo Small' className='logo-small' />
+            </div>
+
+            <form className='search-container' onSubmit={handleSearchSubmit}>
+                <input
+                    className='search-input'
+                    type='text'
+                    value={searchTerm}
+                    onChange={handleInputChange}
+                    placeholder='Search...'
+                />
+                <button className='search-btn'type='submit'>🔍</button>
+            </form>
+        </div>
+    );
+};
+
+export default Header;
