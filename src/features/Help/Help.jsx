8e59f634eff56d469e56606891f97e4f6ec63804
Sidney Russell <sidneyrussell58@gmail.com>
2025-08-14 19:04:21 -0700

    Updated Header import location

--- src/features/Help/Help.jsx
+++ src/features/Help/Help.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../Header/Header';
+import Header from '../../components/Header/Header';
 import './Help.css';
 
 const Help = () => {
--- src/features/Settings/Settings.jsx
+++ src/features/Settings/Settings.jsx
@@ -1,6 +1,6 @@
 //Make the settings usable
 import React, { useState } from 'react';
-import Header from '../Header/Header';
+import Header from '../../components/Header/Header';
 import './Settings.css';
 
 const Settings = () => {
