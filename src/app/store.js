0ed7ba312a46c09e3c8daab587e381b4d0f151d8
Sidney Russell <sidneyrussell58@gmail.com>
2025-09-02 16:01:46 -0700

    Updated store to fit new file move

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postReducer from '../features/post/postSlice';
 import submedditReducer from '../features/submeddits/submedditSlice';
-import subRedditReducer from '../store/subRedditSlice';
+import subRedditReducer from '../features/submeddits/subRedditSlice';
 import userReducer from '../features/user/userSlice';
 import searchReducer from '../features/search/searchSlice';
 
