70931610829eb8c24c3bcef24893d3675134ea4e
Sidney Russell <sidneyrussell58@gmail.com>
2025-09-02 16:30:15 -0700

    Updated store to include redditSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postReducer from '../features/post/postSlice';
+import redditReducer from '../app/redditSlice';
 import subRedditReducer from '../features/submeddits/subRedditSlice';
 import userReducer from '../features/user/userSlice';
 import searchReducer from '../features/search/searchSlice';
@@ -7,6 +8,7 @@ import searchReducer from '../features/search/searchSlice';
 const store = configureStore({
     reducer: {
         post: postReducer,
+        reddit: redditReducer,
         subreddits: subRedditReducer, //API
         user: userReducer,
         search: searchReducer,
