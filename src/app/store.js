a1368ed877244dbd93f84616c706595f92ad6ab0
Sidney Russell <sidneyrussell58@gmail.com>
2025-09-07 11:39:13 -0700

    Updated store to reflect slice changes

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import postReducer from '../features/post/postSlice';
 import redditReducer from '../app/redditSlice';
-import subRedditReducer from '../features/submeddits/subRedditSlice';
 import userReducer from '../features/user/userSlice';
-import searchReducer from '../features/search/searchSlice';
 
 const store = configureStore({
     reducer: {
-        post: postReducer,
         reddit: redditReducer,
         subreddits: subRedditReducer, //API
         user: userReducer,
-        search: searchReducer,
     },
 });
 
