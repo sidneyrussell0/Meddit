69ac22e0672a126f22a207fcb148d9b6be3ef3a4
Sidney Russell <sidneyrussell58@gmail.com>
2025-09-07 11:53:13 -0700

    Updated Submeddits component to work with redditSlice

--- src/features/Submeddits/Submeddits.jsx
+++ src/features/Submeddits/Submeddits.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchSubreddits } from './subRedditSlice';
+import { fetchSubreddits } from '../../app/redditSlice';
 import './Submeddits.css';
 
 const Submeddits = () => {
