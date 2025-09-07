a0cde331e5f02f732ce85304a49afcf410df4771
Sidney Russell <sidneyrussell58@gmail.com>
2025-09-07 11:36:40 -0700

    Updated search result component to work with redditSlice

--- src/features/search/SearchResults.jsx
+++ src/features/search/SearchResults.jsx
@@ -2,23 +2,25 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { fetchSearchResults } from './searchSlice';
+import { fetchSearchResults, setSearchTerm } from '../../app/redditSlice';
 
 
 function SearchResults() {
     const { query } = useParams();
     const dispatch = useDispatch();
-    const { results, loading, error } = useSelector((state) => state.search);
+    
+    const { searchResults, loading, error } = useSelector((state) => state.reddit);
 
     useEffect(() => {
         if (query) {
+            dispatch(setSearchTerm(query));
             dispatch(fetchSearchResults(query));
         }
     }, [query, dispatch]);
 
     if (loading) return <p>Loading results...</p>;
     if (error) return <p>Error: {error}</p>;
-    if (!results.length) return <p>No results found for "{query}"</p>;
+    if (!searchResults.length) return <p>No results found for "{query}"</p>;
 
     return (
         <div className='search-results'>
@@ -26,7 +28,7 @@ function SearchResults() {
             {results.map((post) => (
                 <div key={post.id} className="post-card">
                 <h3>{post.title}</h3>
-                <p>{post.selftext}</p>
+                <p>{post.selftext || post.body}</p>
                 <small>Posted by {post.author}</small>
               </div>
             ))}
